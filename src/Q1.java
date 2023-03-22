78551ac30ecbf9d338e51d2dfd4c895e68cd300b
ryanhabis <ryan.habiss@gmail.com>
2023-03-22 11:02:49 +0000

    Collections Exercise upload

--- src/Q1.java
+++ src/Q1.java
@@ -11,5 +11,75 @@ public class Q1
         //      If a word appears more than once in a single line then the line number should only be output once.
         Scanner scanner = new Scanner(new File("data.txt"));
         Map<String, Set<Integer>> fWord =  new HashMap<>();
+
+        int lCount = 0;
+        String line;
+        String[] aLine = new String[0];
+        while (scanner.hasNext())
+        {
+            line = scanner.next();
+            aLine = line.split(" , ");
+            for (int i = 0; i < aLine.length; i++)
+            {
+                if (!fWord.containsKey(aLine[i]))
+                {
+                    fWord.put(aLine[i],new HashSet<>());
+                }
+                (fWord.get(aLine[i])).add(lCount);
+
+            }
+            lCount++;
+        }
+        System.out.println(fWord.toString());
+
+        System.out.println("End of Q1");
+
+        // Extra exercise
+        // Display the word that appears on most lines.
+        String mostCommonWord = "";
+        int mostCommonCount = 0;
+
+        for (Map.Entry<String, Set<Integer>> entry : fWord.entrySet())
+        {
+            String word = entry.getKey();
+            Set<Integer> lines = entry.getValue();
+
+            if (lines.size() > mostCommonCount)
+            {
+                mostCommonCount = lines.size();
+                mostCommonWord = word;
+            }
+        }
+        System.out.println("The word that appears on the most lines is: " + mostCommonWord);
+
+
+        // Words that appear on more than x lines.
+        int x = 3;
+        StringBuilder moreThanXLines = new StringBuilder();
+
+        for (Map.Entry<String, Set<Integer>> entry : fWord.entrySet()) {
+            String word = entry.getKey();
+            Set<Integer> lines = entry.getValue();
+
+            if (lines.size() > x) {
+                moreThanXLines.append(word).append(" ");
+            }
+        }
+        System.out.println("Words that appear on more than " + x + " lines: " + moreThanXLines);
+
+        // All words in reverse alphabetical order that appears on x line
+
+        String source = "abc";
+        char charArray[] = source.toCharArray();
+
+        Arrays.sort(charArray);
+
+        for (String part : source.split(" ")) {
+            System.out.print(new StringBuilder(part).reverse().toString());
+            System.out.print(" ");
+        }
+
+
+        // Display the words that appear on both lines x and y, either line
     }
 }
