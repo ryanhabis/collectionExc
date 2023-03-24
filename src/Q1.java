efa782eda082c8d5eb9d7e369fad8a14bc3d8ca0
ryanhabis <ryan.habiss@gmail.com>
2023-03-24 09:58:24 +0000

    Collections Exercise uploaded version

--- src/Q1.java
+++ src/Q1.java
@@ -2,84 +2,90 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class Q1
-{
+public class Q1 {
+
     public static void main(String[] args) throws FileNotFoundException
     {
-        //1.	Write an application that reads in a number of lines from a file, giving each line a line number.
-        //      Using a HashMap class, generate an index showing the lines on which each word in the file exists.
-        //      If a word appears more than once in a single line then the line number should only be output once.
-        Scanner scanner = new Scanner(new File("data.txt"));
-        Map<String, Set<Integer>> fWord =  new HashMap<>();
+        Map<String, Set<Integer>> fWords = popMap();
+        //  1.	Write an application that reads in a number of lines
+        //from a file, giving each line a line number.
+        //Using a HashMap class, generate an index showing the lines on
+        //which each word in the file exists.
+        //If a word appears more than once in a single
+        //line then the line number should only be output once.
 
-        int lCount = 0;
+//                System.out.println((int)'T');
+
+    }
+    private static Map<String,Set<Integer>>popMap() throws FileNotFoundException{
+        Scanner sc = new Scanner(new File("data.txt"));
+        Map<String, Set<Integer>> fWords = new HashMap();
+        int lCount = 1;
         String line;
-        String[] aLine = new String[0];
-        while (scanner.hasNext())
-        {
-            line = scanner.next();
-            aLine = line.split(" , ");
-            for (int i = 0; i < aLine.length; i++)
-            {
-                if (!fWord.containsKey(aLine[i]))
-                {
-                    fWord.put(aLine[i],new HashSet<>());
+        String[] aLine;
+        while (sc.hasNext()) {
+            line = sc.nextLine();
+            aLine = line.split(" ");
+            for (int i = 0; i < aLine.length; i++) {
+                if (!fWords.containsKey(aLine[i])) {
+                    fWords.put(aLine[i], new HashSet());
                 }
-                (fWord.get(aLine[i])).add(lCount);
-
+                (fWords.get(aLine[i])).add(lCount);
             }
             lCount++;
         }
-        System.out.println(fWord.toString());
-
-        System.out.println("End of Q1");
-
-        // Extra exercise
-        // Display the word that appears on most lines.
-        String mostCommonWord = "";
-        int mostCommonCount = 0;
-
-        for (Map.Entry<String, Set<Integer>> entry : fWord.entrySet())
-        {
-            String word = entry.getKey();
-            Set<Integer> lines = entry.getValue();
-
-            if (lines.size() > mostCommonCount)
-            {
-                mostCommonCount = lines.size();
-                mostCommonWord = word;
-            }
-        }
-        System.out.println("The word that appears on the most lines is: " + mostCommonWord);
-
-
-        // Words that appear on more than x lines.
-        int x = 3;
-        StringBuilder moreThanXLines = new StringBuilder();
-
-        for (Map.Entry<String, Set<Integer>> entry : fWord.entrySet()) {
-            String word = entry.getKey();
-            Set<Integer> lines = entry.getValue();
-
-            if (lines.size() > x) {
-                moreThanXLines.append(word).append(" ");
-            }
-        }
-        System.out.println("Words that appear on more than " + x + " lines: " + moreThanXLines);
-
-        // All words in reverse alphabetical order that appears on x line
-
-        String source = "abc";
-        char charArray[] = source.toCharArray();
-
-        Arrays.sort(charArray);
-
-        for (String part : source.split(" ")) {
-            System.out.print(new StringBuilder(part).reverse().toString());
-            System.out.print(" ");
-        }
-
-
-        // Display the words that appear on both lines x and y, either line
+        return fWords;
     }
+    //Any word that appears on most lines
+    private static String mostUsed(Map<String, Set<Integer>> fWords) {
+        int max = 0;
+        String mWord = "";
+        for (Map.Entry<String, Set<Integer>>element : fWords.entrySet()) {
+            if(element.getValue().size()>max){
+                max = element.getValue().size();
+                mWord = element.getKey();
+            }
+
+        }
+        return mWord;
+    }
+    //Words that appear on more than x lines
+    private static Set<String> moreXLines(Map<String, Set<Integer>> fWords,int x) {
+
+
+        Set<String> words = new HashSet<>();
+        for (Map.Entry<String, Set<Integer>>element : fWords.entrySet()) {
+            if(element.getValue().size()>x){
+                words.add(element.getKey());
+
+            }
+
+        }
+        return words;
+    }
+    //All words in reverse alpahabetical order that appear on line x
+    private static Set<String> getLineXWords(Map<String, Set<Integer>> fWords,int x) {
+
+
+        Set<String> words = new TreeSet<>(new RevStringComp());
+        for (Map.Entry<String, Set<Integer>>element : fWords.entrySet()) {
+            if(element.getValue().contains(x)){
+                words.add(element.getKey());
+
+            }
+
+        }
+        return words;
+    }
+    //Display words that appear on both line x and y, either line x or y, on line x but not y
+    private static void commonWords(Map<String, Set<Integer>> fWords,int x, int y)
+    {
+        Set<String> lineX = getLineXWords(fWords, x);
+        Set<String> lineY = getLineXWords(fWords, y);
+        Set<String> common = new HashSet<>(lineX);
+        common.retainAll(lineY);
+        System.out.println("Words on both line " + x + " and line " + y + ": " + common);
+
+    }
+
 }
